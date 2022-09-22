753a7d657b7f706beb6cecd35aed03823e136016
joshuakrempasky <joshkrempasky@gmail.com>
2022-09-22 13:10:27 -0400

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,21 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+
+        Scanner kb = new Scanner(System.in);
+        String partyAfil;
+
+        System.out.println("Are you a [R]epublican, [D]emocrat, or [I]ndependent?");
+        partyAfil = kb.nextLine();
+
+        System.out.println(partyAfil);
+
+
+
+
+
+
+
+    }
+}
